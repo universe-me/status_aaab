838f93b813bb2297bcfcd9f79c7c2404c9e9175a
universe-me <31508316+universe-me@users.noreply.github.com>
2021-02-06 09:54:31 +0800

    Add files via upload

---
+++ uptimerobot/js/config.js
@@ -0,0 +1,11 @@
+var __apiKeys = [
+	'key复制在这里', //1
+	'key复制在这里', //2
+	'key复制在这里', //3
+
+];
+//https://uptimerobot.com/ 设置要监控的域名或者ip 获取到key
+
+
+// refresh interval (in seconds)
+var __refresh = 180;
