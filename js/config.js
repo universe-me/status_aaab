afcf00749dcd0b224802bf62942aabde37d3f75a
universe-me <31508316+universe-me@users.noreply.github.com>
2021-04-28 20:30:06 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,7 @@ var __apiKeys = [
 	'm787151641-ce1220f2f66155424233b319',
 	'm787151895-6ca72f1362bf2557eed9064f',
 	'm787151887-f0f8fbaaed8581437780a434',
+	'm787986932-0f3ad748d7f7335bafc5acdc',
 
 ];
 //https://uptimerobot.com/ 设置要监控的域名或者ip 获取到key
